4de6d5ff95c06b0b0bf0e1cc733e420e3e61ff7b
Polizzi12 <peppepolizzi73@gmail.com>
2024-05-07 12:36:41 +0200

    cm3

--- wordpress-react/src/components/post.jsx
+++ wordpress-react/src/components/post.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const Post = ({ title, content, author, date }) => {
   return (
     <div className="post" style={{ backgroundColor: '#007bff', color: '#ffffff' }}>
+     <div>
       <h2>{title}</h2>
-      {content}
+     {content}
+     </div>
       <div className="post-details">
-        <p><strong>Autore:</strong> {author}</p>
-        <p><strong>Data di pubblicazione:</strong> {date}</p>
+        <p style={{ color: '#1A3CC1' }}><strong>Autore:</strong> <span style={{ color: 'white' }}>{author}</span></p>
+        <p style={{ color: '#1A3CC1' }}><strong>Data di pubblicazione:</strong> <span style={{ color: 'white' }}>{date}</span></p>
       </div>
     </div>
   );
